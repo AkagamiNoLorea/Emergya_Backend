4468657c39496cb0549f6f6adff8923ccb7b2dfd
AkagamiNoLorea <raiseta92@gmail.com>
2023-10-04 11:00:30 +0200

    crear base proyecto

---
+++ src/test/java/Emergya/Emargya_B/EmargyaBApplicationTests.java
@@ -0,0 +1,13 @@
+package Emergya.Emargya_B;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmargyaBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
