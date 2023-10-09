3e82a3e470edb87ea2a1469d6306eeeae7807041
Recoded13 <adsanji@gmail.com>
2023-10-09 09:58:26 +0200

    actualizacion path

--- src/main/java/Emergya/Emergya_B/apiControllers/EstadoController.java
+++ src/main/java/Emergya/Emergya_B/apiControllers/EstadoController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "api/v1/estado")
+@RequestMapping(path = "/estado")
 public class EstadoController {
 
     private final EstadoService estadoService;
