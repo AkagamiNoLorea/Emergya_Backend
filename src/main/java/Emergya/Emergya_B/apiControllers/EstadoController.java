9e6ce47a132daeec6e8fc180e12cb418e8194efc
Recoded13 <adsanji@gmail.com>
2023-10-09 09:55:37 +0200

    Crud Estado y algunas cosas de application.properties

--- src/main/java/Emergya/Emergya_B/apiControllers/EstadoController.java
+++ src/main/java/Emergya/Emergya_B/apiControllers/EstadoController.java
@@ -1,4 +1,44 @@
 package Emergya.Emergya_B.apiControllers;
 
+import Emergya.Emergya_B.domain.models.Estado;
+import Emergya.Emergya_B.domain.services.EstadoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/estado")
 public class EstadoController {
+
+    private final EstadoService estadoService;
+
+    @Autowired
+
+    public EstadoController(EstadoService estadoService){
+        this.estadoService = estadoService;
+
+    }
+
+    @GetMapping
+    public List<Estado> getBookController(){ return  estadoService.getBook();
+    }
+
+    @PostMapping
+    public void createbook(@RequestBody Estado estado){
+        estadoService.newEstado(estado);
+    }
+
+    @PutMapping("/{id}")
+    public void updateBook(@PathVariable Integer id, @RequestBody Estado estado){
+        estadoService.update(id, estado);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id){
+       estadoService.deletebook(id);
+
+    }
+
 }
