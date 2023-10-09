7f4c671891517a577d63373f7d8730430d69df33
xavierlocre <xavilok@gmail.com>
2023-10-09 11:57:37 +0200

    se creo el modelo del puesto

--- src/main/java/Emergya/Emergya_B/infrarepositorie/PuestoRepository.java
+++ src/main/java/Emergya/Emergya_B/infrarepositorie/PuestoRepository.java
@@ -1,7 +1,7 @@
 package Emergya.Emergya_B.infrarepositorie;
 /*import org.springframework.stereotype.Repository;*/
-import Emergya.Emergya_B.domain.models.Asientos;
+import Emergya.Emergya_B.domain.models.Puesto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PuestoRepository extends JpaRepository<Asientos, Integer> {
+public interface PuestoRepository extends JpaRepository<Puesto, Integer> {
 }
