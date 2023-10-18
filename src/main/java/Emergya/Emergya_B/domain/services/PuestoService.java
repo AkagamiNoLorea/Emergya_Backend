cd4e706c7de35f400cee781c69f5d4782f9e1cc0
mohamed ali hmaidi <mohamedalihmaidi@MacBook-Pro-de-mohamed.local>
2023-10-18 14:27:34 +0200

    Actualizar relacion PuestoService

--- src/main/java/Emergya/Emergya_B/domain/services/PuestoService.java
+++ src/main/java/Emergya/Emergya_B/domain/services/PuestoService.java
@@ -18,29 +18,28 @@ public class PuestoService {
         this.puestoRepository = puestoRepository;
     }
 
-    // Método para obtener los puestos disponibles.
+
     public List<Puesto> obtenerPuestosDisponibles() {
-        // Asumo que tienes un método en tu repository que te devuelve los puestos disponibles.
-        // Si no lo tienes, deberás crear uno.
+
         return puestoRepository.findAllByDisponibleTrue();
     }
 
-    // Método para guardar un nuevo puesto.
+
     public Puesto guardarPuesto(Puesto puesto) {
         return puestoRepository.save(puesto);
     }
 
-    // Método para obtener todos los puestos.
+
     public List<Puesto> obtenerTodosLosPuestos() {
         return puestoRepository.findAll();
     }
 
-    // Método para obtener un puesto específico por su ID.
+
     public Optional<Puesto> obtenerPuestoPorId(Long id) {
         return puestoRepository.findById(id);
     }
 
-    // Método para eliminar un puesto por su ID.
+
     public void eliminarPuesto(Long id) {
         puestoRepository.deleteById(id);
     }
