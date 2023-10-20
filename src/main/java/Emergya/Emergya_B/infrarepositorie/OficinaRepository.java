aaee4fbe5b5387cfd5694438a76d579ae25f5536
AkagamiNoLorea <raiseta92@gmail.com>
2023-10-20 10:47:59 +0200

    ....

--- src/main/java/Emergya/Emergya_B/infrarepositorie/OficinaRepository.java
+++
@@ -1,7 +0,0 @@
-package Emergya.Emergya_B.infrarepositorie;
-/*import org.springframework.stereotype.Repository;*/
-import Emergya.Emergya_B.domain.models.Oficina;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OficinaRepository extends JpaRepository<Oficina, Integer> {
-}
--- src/main/java/Emergya/Emergya_B/infrarepositorie/UsuariopuestoRepository.java
+++
@@ -1,8 +0,0 @@
-package Emergya.Emergya_B.infrarepositorie;
-
-import Emergya.Emergya_B.domain.models.Oficina;
-import Emergya.Emergya_B.domain.models.Usuariopuesto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuariopuestoRepository extends JpaRepository <Usuariopuesto, Integer> {
-}
